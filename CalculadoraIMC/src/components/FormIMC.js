ba890aa9678fd9744b1cbaa8c8f3fb18c1c62f7e
CARLOS ALBERTO DA SILVA JUNIOR <carlos.junior@etec.sp.gov.br>
2025-04-03 15:25:12 -0300

    Base

--- CalculadoraIMC/src/components/FormIMC.js
+++ CalculadoraIMC/src/components/FormIMC.js
@@ -1,6 +1,6 @@
 import { View, TextInput, Button, StyleSheet } from "react-native";
 import Result from "./result";
-
+import React, { useState } from "react";
 const FormIMC = () => {
     const [peso, setPeso] = useState("");
     const [altura, setAltura] = useState("");
